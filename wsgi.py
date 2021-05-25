c2a7200937f47b75a663a1fc17a633ec5869f426
VladyslavPodrazhanskyi <https://VladyslavPodrazhanskyi@github.com>
2021-05-25 22:50:19 +0300

    departmentsemployees

--- wsgi.py
+++ wsgi.py
@@ -3,11 +3,6 @@
 import os
 from flask_migrate import Migrate
 from src import create_app, db
-from src.insert_db import populate_departments, populate_employees
-
 
 app = create_app(os.environ.get('FLASK_CONFIG'))
 migrate = Migrate(app, db)
-
-populate_departments()
-populate_employees()
