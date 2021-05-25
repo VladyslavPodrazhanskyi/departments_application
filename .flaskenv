d627bccb79df6318c37828a4bade6d638ac30856
VladyslavPodrazhanskyi <https://VladyslavPodrazhanskyi@github.com>
2021-05-25 23:18:50 +0300

    departmentsemployees

--- .flaskenv
+++ .flaskenv
@@ -1,3 +1,3 @@
-FLASK_CONFIG=development
+FLASK_CONFIG=production
 SECRET_KEY=b'\x11\xd0\x8d\x06\xc0\x94@*\x8fv&\x84\x89\x84<lO[{\xb1>\xde\xa4n'
 
--- config.py
+++ config.py
@@ -27,8 +27,8 @@ class DevelopmentConfig(Config):
     PG_HOST = "localhost"
     PG_PORT = 5432
     DB_NAME = "departments_db"
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') #or \
-    #                           f"postgresql://{PG_USER}:{PG_PASSWORD}@{PG_HOST}:{PG_PORT}/{DB_NAME}"
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+                              f"postgresql://{PG_USER}:{PG_PASSWORD}@{PG_HOST}:{PG_PORT}/{DB_NAME}"
 
 
 class TestingConfig(Config):
