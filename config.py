b201bb544208b1f34f7a875e081aed83f703d38c
VladyslavPodrazhanskyi <https://VladyslavPodrazhanskyi@github.com>
2021-05-27 00:14:21 +0300

    updated application deployed to heroku

--- config.py
+++ config.py
@@ -30,7 +30,7 @@ class TestingConfig(Config):
 
 class ProductionConfig(Config):
     DEBUG = False
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace("://", "ql://", 1)
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 
 
 config = {
